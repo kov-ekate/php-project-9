7383dc352f64d02ee8ce8abb637cfa3026acaec5
Екатерина Коврижко <eh1306@yandex.ru>
2025-08-27 13:46:31 +0300

    add templates

--- public/index.php
+++ public/index.php
@@ -2,13 +2,20 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Views\PhpRenderer;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Hello, World!');
-    return $response;
+    $params = [];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
 });
 
 $app->run();
