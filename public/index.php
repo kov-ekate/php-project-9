49a0f8f856b31ab49536ca040f1c86e77e46990a
Екатерина Коврижко <eh1306@yandex.ru>
2025-08-29 14:23:40 +0300

    fix index.php

--- public/index.php
+++ public/index.php
@@ -14,10 +14,12 @@ $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    error_log('render / once');
-    return $response->withHeader('Content-Length', '0');
+    $method = $request->getMethod();
+    if ($method === 'HEAD') {
+        return $response->withHeader('Content-Length', '0')->withStatus(200);
+    }
+    $params = [];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
 });
-
-
 
 $app->run();
